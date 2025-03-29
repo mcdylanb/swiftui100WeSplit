c4157a85c34a349943065372a4f34a0add1a466c
Dylan Balagtas <dylansatgalab@gmail.com>
2025-03-29 18:27:26 +0800

    Initial Commit

---
+++ WeSplitTests/WeSplitTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitTests.swift
+//  WeSplitTests
+//
+//  Created by Dylan Balagtas on 3/29/25.
+//
+
+import Testing
+@testable import WeSplit
+
+struct WeSplitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
