19e4580ce96f12f51d88f7c3c24d42358992f815
Dylan Balagtas <dylansatgalab@gmail.com>
2025-03-31 01:06:30 +0800

    adds currency base locality and changes keyboard upon edit

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 20
     var body: some View {
-        Text("Hello World!")
+        let tipPercentage = [10,15,20,25,0]
+        Form{
+            Section{
+                TextField("Amount:", value: $checkAmount, format:
+                        .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                .keyboardType(.decimalPad)
+            }
+            Section{
+                Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+            }
+        }
     }
 }
 
